ee98ee6852a99435353f6252539641157993bd00
Martin Hamrle <martin.hamrle@monetas.net>
2014-10-08 08:42:41 +0200

    more tests for cheque

--- python3/tests/test_client_server.py
+++ python3/tests/test_client_server.py
@@ -1,4 +1,6 @@
 import pyopentxs
+import unittest
+import pytest
 from datetime import datetime, timedelta
 
 # def test_check_server_id():
@@ -23,34 +25,97 @@ def test_issue_asset_contract():
     server_id = pyopentxs.first_server_id()
     pyopentxs.issue_asset_type(server_id, nym_id, open(btc_contract_file))
 
+class Account(object):
+    def __init__(self, nym_id):
+        self.nym_id = nym_id
+        self.account_id = None
 
-def test_issue_write_cheque():
-    server_id = pyopentxs.first_server_id()
+class TestWriteCheque(unittest.TestCase):
+    def setup_method(self, method):
+        self.server_id = pyopentxs.first_server_id()
 
-    nym_target_id = register_new_nym()
+        self.source = Account(register_new_nym())
+        self.target = Account(register_new_nym())
 
-    nym_id = register_new_nym()
-    asset = pyopentxs.issue_asset_type(server_id, nym_id, open(btc_contract_file))
+        self.issuer = Account(register_new_nym())
+        self.asset = pyopentxs.issue_asset_type(self.server_id, self.issuer.nym_id, open(btc_contract_file))
 
-    account_id = pyopentxs.create_account(server_id, nym_id, asset.asset_id)
-    target_account_id = pyopentxs.create_account(server_id, nym_target_id, asset.asset_id)
+        self.target.account_id = pyopentxs.create_account(self.server_id, self.target.nym_id, self.asset.asset_id)
+        self.source.account_id = pyopentxs.create_account(self.server_id, self.source.nym_id, self.asset.asset_id)
+        self.issuer.account_id = self.asset.issuer_account_id
 
-    # check that both account has zero balance
-    assert 0 == pyopentxs.get_account_balance(server_id, nym_id, asset.issuer_account_id)
-    assert 0 == pyopentxs.get_account_balance(server_id, nym_target_id, target_account_id)
+        #send 100 from issuer to nym_source_id
+        self.transfer(100, source=self.issuer, target=self.source)
 
-    # create and deposit check
-    now = datetime.utcnow()
-    cheque = pyopentxs.Cheque(server_id, 10, now + timedelta(0, -1), now + timedelta(0, 1000),
-                              asset.issuer_account_id, nym_id, "memo", nym_target_id)
+        # check that all account has expected balance
+        self.check_balance(-100, 100, 0)
 
-    cheque.write()
-    deposit = cheque.deposit(nym_target_id, target_account_id)
-    assert pyopentxs.is_message_success(deposit)
+    def check_balance(self, issuer, source, target):
+        assert issuer == pyopentxs.get_account_balance(self.server_id, self.issuer.nym_id, self.issuer.account_id)
+        assert source == pyopentxs.get_account_balance(self.server_id, self.source.nym_id, self.source.account_id)
+        assert target == pyopentxs.get_account_balance(self.server_id, self.target.nym_id, self.target.account_id)
 
-    # check that balance is changed
-    assert -10 == pyopentxs.get_account_balance(server_id, nym_id, asset.issuer_account_id)
-    assert 10 == pyopentxs.get_account_balance(server_id, nym_target_id, target_account_id)
+    def transfer(self, amount, source=None, target=None, valid_from = -1, valid_to=1000, valid=True):
+        if not source:
+            source = self.source
+        if not target:
+            target = self.target
+        now = datetime.utcnow()
+        cheque = pyopentxs.Cheque(self.server_id, amount, now + timedelta(0, valid_from), now + timedelta(0, valid_to),
+                      source.account_id, source.nym_id, "memo", target.nym_id)
+
+        cheque.write()
+        deposit = cheque.deposit(target.nym_id, target.account_id)
+        if valid:
+            assert pyopentxs.is_message_success(deposit)
+        else:
+            with pytest.raises(pyopentxs.ReturnValueError):
+                pyopentxs.is_message_success(deposit)
+        return cheque
+
+    def test_one_deposit(self):
+        self.transfer(10)
+        self.check_balance(-100, 90, 10)
+
+    def test_not_enough_funds(self):
+        #first attempt fails because source does not have enough funds
+        cheque = self.transfer(200, valid=False)
+        self.check_balance(-100, 100, 0)
+        
+        #now transfer more funds to source
+        self.transfer(100, source=self.issuer, target=self.source)
+        #and repeat cheque deposit
+        deposit = cheque.deposit(self.target.nym_id, self.target.account_id)
+        assert pyopentxs.is_message_success(deposit)
+
+        self.check_balance(-200, 0, 200)
+
+    def test_two_deposits(self):
+        cheque = self.transfer(10)
+
+        # check balance after first transfer
+        self.check_balance(-100, 90, 10)
+
+        # second deposit should fail
+        deposit = cheque.deposit(self.target.nym_id, self.target.account_id)
+        with pytest.raises(pyopentxs.ReturnValueError):
+            pyopentxs.is_message_success(deposit)
+
+        # check that balance is not changed by second deposit
+        self.check_balance(-100, 90, 10)
+
+    def test_expired_cheque(self):
+        self.transfer(10, valid_from=-100, valid_to=-1, valid=False)
+        self.check_balance(-100, 100, 0)
+
+    def test_not_yet_valid_cheque(self):
+        self.transfer(10, valid_from=10, valid_to=100, valid=False)
+        self.check_balance(-100, 100, 0)
+
+    def test_negative_transfer(self):
+        self.transfer(-10, valid=False)
+        self.check_balance(-100, 100, 0)
+        
 
 def test_create_account():
     server_id = pyopentxs.first_server_id()
